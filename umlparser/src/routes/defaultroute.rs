27e5126b4a5d2bc955a6a455bfb410c32729eae8
Leon Hornig <Leon.Hornig@web.de>
2018-11-09 23:20:37 +0100

    Added Base

--- umlparser/src/routes/defaultroute.rs
+++ umlparser/src/routes/defaultroute.rs
@@ -8,5 +8,5 @@ pub fn index() -> Template {
     let mut context = Context::new();
 
     context.add("my_message", &"Heya from template context!");
-    Template::render("index", &context)
+    Template::render("content", &context)
 }
